1903c4a78bcfb6c8e25823dc17493999142fd817
tshepoMas <tshepo.mas@gmail.com>
2017-01-07 21:39:19 +0200

    My hello world reading a web page and also producing audio

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var request = require("request"),
   cheerio = require("cheerio"),
   url = "https://en.wikipedia.org/wiki/Issac_Newton";
+
+var googleTTS = require('google-tts-api');
   
 request(url, function (error, response, body) {
   if (!error) {
@@ -11,9 +13,17 @@ request(url, function (error, response, body) {
     
     text = text.replace(/\s+/g, " ")
        .replace(/[^a-zA-Z ]/g, "")
-       .toLowerCase();
+       .toLowerCase'();'
     
-    console.log(text);
+    //console.log(text);
+      
+      googleTTS('Hello my friends', 'en', 1)   // speed normal = 1 (default), slow = 0.24 
+        .then(function (url1) {
+          console.log(url1); // https://translate.google.com/translate_tts?... 
+        })
+        .catch(function (err) {
+          console.error(err.stack);
+        });
     
   } else {
     console.log("We’ve encountered an error: " + error);
